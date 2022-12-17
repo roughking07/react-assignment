58c6ab2feeda99e7a2d82ef9480b74ddd5b1ece7
Kunal Khatri <kunalkhatri017@gmail.com>
2022-12-17 12:13:14 +0530

    Assignment updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Navbarhome />
     &nbsp;&nbsp;<span style={{'font-size':12}}> Medical Darpan{'>'} Search {'>'} <span style={{'font-weight':'700'}}> FabiFlu Tablet </span></span>
     <Search />
-    <span style={{'font-size':26}}>&nbsp;&nbsp; Paracetamol </span><span> (128 products) </span> <span style={{'background-color':'teal','color':'white'}}> Aceclofenac <i class="fa-solid fa-xmark"></i> &nbsp;
+    <span style={{'font-size':26}}>&nbsp;&nbsp; Paracetamol </span>&nbsp;&nbsp;<span style={{'font-size':12}}> (128 products) </span> &nbsp;<span style={{'background-color':'teal','color':'white'}}> Aceclofenac <i class="fa-solid fa-xmark"></i> &nbsp;
      </span> &nbsp;<span style={{'background-color':'teal','color':'white'}}> 500 mg <i class="fa-solid fa-xmark"></i>&nbsp; </span><span style={{'color':'maroon','font-size':14}}>&nbsp;&nbsp;&nbsp; Remove All</span>
     <div className='container' >
       <Sidebar />
